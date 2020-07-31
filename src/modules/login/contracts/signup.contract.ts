ed222f2900d4047d61a1433eb49891a2df5fdb91
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2020-07-31 02:05:19 -0400

    convention name for private variables

--- src/modules/login/contracts/signup.contract.ts
+++ src/modules/login/contracts/signup.contract.ts
@@ -1,18 +1,18 @@
 import { Contract } from '../../../infra/models/contract';
 import { Notifiable } from '../../../infra/models/notifiable';
-import { SignupDto } from '../dtos/signup.dto';
 import { Validator } from '../../../infra/validator/validator';
+import { SignupDto } from '../dtos/signup.dto';
 
 
 export class SignupContract extends Notifiable implements Contract {
 
-    private dto: SignupDto;
-    private validator: Validator;
+    private _dto: SignupDto;
+    private _validator: Validator;
 
     constructor(dto: SignupDto) {
         super();
-        this.dto = dto;
-        this.validator = new Validator();
+        this._dto = dto;
+        this._validator = new Validator();
     }
 
     validate(): boolean {
@@ -21,37 +21,37 @@ export class SignupContract extends Notifiable implements Contract {
         this.validatePassword();
         this.validateConfirmPassword();
 
-        this.addReports(this.validator.reports);
+        this.addReports(this._validator.reports);
         return this.isValid();
     }
 
 
     private validateName() {
-        this.validator.isRequired(this.dto.name, 'name', 'name is required');
+        this._validator.isRequired(this._dto.name, 'name', 'name is required');
     }
 
     private validateEmail() {
-        const { email } = this.dto;
-        this.validator.isRequired(email, 'email', 'email is required');
-        this.validator.isValidEmail(email, 'email', 'invalid email');
+        const { email } = this._dto;
+        this._validator.isRequired(email, 'email', 'email is required');
+        this._validator.isValidEmail(email, 'email', 'invalid email');
     }
 
     private validatePassword() {
-        const { password } = this.dto;
-        this.validator.isRequired(password, 'password', 'password is required');
+        const { password } = this._dto;
+        this._validator.isRequired(password, 'password', 'password is required');
 
         if (password) {
             const MIN_PASSWORD_LENGTH = 6;
-            this.validator.isLessThan(password.length, MIN_PASSWORD_LENGTH,
+            this._validator.isLessThan(password.length, MIN_PASSWORD_LENGTH,
                 'password', `password must be at least 6 characters`,
             );
         }
     }
 
     private validateConfirmPassword() {
-        this.validator.isRequired(this.dto.confirmPassword, 'confirmPassword', 'confirmPassword is required');
+        this._validator.isRequired(this._dto.confirmPassword, 'confirmPassword', 'confirmPassword is required');
 
-        if (this.dto.password != this.dto.confirmPassword) {
+        if (this._dto.password != this._dto.confirmPassword) {
             this.addReport({ name: 'confirmPassword', message: 'confirm password not match with password' });
         }
     }

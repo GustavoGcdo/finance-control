646e5d74f45d378736ef2c2f1a324440ee08cc8c
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2020-08-24 19:49:33 -0400

    get user from token when add operation

--- tests/integration/finance/addOperation.test.ts
+++ tests/integration/finance/addOperation.test.ts
@@ -23,31 +23,11 @@ describe('Add Operation a User', () => {
         await app.disconnect();
     });
 
-    it('POST - must return fail when trying to add operation a invalid user', async () => {
-        const { tokenToTest } = await createAndLoginUser();
-
-        const operationToAdd = {
-            type: 'recipe',
-            userId: 'userIdInvalid',
-            value: 50.0
-        } as AddOperationDto;
-
-        const result = await request(expressAplication)
-            .post('/finance/operations')
-            .send(operationToAdd)
-            .set('Authorization', 'Bearer ' + tokenToTest);
-        expect(result.status).toEqual(HttpStatus.BAD_REQUEST);
-        expect(result.body.message).toEqual('fail to add operation');
-        expect(result.body.success).toBeFalsy();
-    });
-
-
     it('POST - must return fail when trying to add operation a invalid value', async () => {
-        const { userToTest, tokenToTest } = await createAndLoginUser();
+        const { tokenToTest } = await createAndLoginUser();
 
         const recipeToAdd = {
             type: 'expense',
-            userId: userToTest._id,
             value: -50.0
         } as AddOperationDto;
 
@@ -79,27 +59,5 @@ describe('Add Operation a User', () => {
         expect(result.body.message).toEqual('fail to add operation');
         expect(result.body.success).toBeFalsy();
         expect(result.body.errors).toContainEqual({ name: 'type', message: 'operation invalid' });
-    });
-
-
-    it('POST - must return failure when trying to add a operation to a non-existent user ', async () => {
-        const { tokenToTest } = await createAndLoginUser();
-        const idValidUserNonExistent = new Types.ObjectId().toHexString();
-
-        const recipeToAdd = {
-            type: 'expense',
-            userId: idValidUserNonExistent,
-            value: 50.0
-        } as AddOperationDto;
-
-        const result = await request(expressAplication)
-            .post('/finance/operations')
-            .send(recipeToAdd)
-            .set('Authorization', 'Bearer ' + tokenToTest);
-
-        expect(result.status).toEqual(HttpStatus.BAD_REQUEST);
-        expect(result.body.message).toEqual('fail to add operation');
-        expect(result.body.success).toBeFalsy();
-        expect(result.body.errors).toContainEqual({ name: 'user', message: 'non-existent user' });
     });
 });

14982f127257ceac2115a81242fc0d774e3c3e2f
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2021-03-02 20:26:14 -0400

    add new test case for get user extract

--- tests/integration/finance/getUserExtract.test.ts
+++ tests/integration/finance/getUserExtract.test.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import request from 'supertest';
 import { App } from '../../../src/app';
+import { AddOperationDto } from '../../../src/modules/operations/dtos/add-operation.dto';
 import { HttpStatus } from '../../../src/presentation/helper/enums/http-status.enum';
 import { createAndLoginUser } from '../../helpers/user-functions-default';
 
@@ -36,5 +37,32 @@ describe('Add Recipe a User', () => {
     expect(body.data).toHaveProperty('balance');
     expect(body.data).toHaveProperty('totalRecipes');
     expect(body.data).toHaveProperty('totalExpenses');
+  });
+
+  it('GET - must return success when fetching a user statement', async () => {
+    const { tokenToTest } = await createAndLoginUser();
+
+    const VALUE_TO_ADD = 50.0;
+
+    const recipeToAdd = {
+      type: 'recipe',
+      value: VALUE_TO_ADD,
+      executed: true
+    } as AddOperationDto;
+
+    await request(expressAplication)
+      .post('/finance/operations')
+      .send(recipeToAdd)
+      .set('Authorization', 'Bearer ' + tokenToTest);
+
+    const result = await request(expressAplication)
+      .get('/finance/extract')
+      .set('Authorization', 'Bearer ' + tokenToTest);
+
+    const { status, body } = result;
+    expect(status).toBe(HttpStatus.SUCCESS);
+    expect(body.data.balance).toBe(VALUE_TO_ADD);
+    expect(body.data.totalRecipes).toBe(VALUE_TO_ADD);
+    expect(body.data.totalExpenses).toBe(0);
   });
 });

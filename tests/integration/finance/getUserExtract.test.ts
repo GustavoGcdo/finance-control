262e4e1c56c69bfdc805db82323c4212ea81be7c
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2021-03-20 13:39:18 -0400

    add new tests for user extracts

--- tests/integration/finance/getUserExtract.test.ts
+++ tests/integration/finance/getUserExtract.test.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import request from 'supertest';
 import { App } from '../../../src/app';
+import { AddOperationDto } from '../../../src/modules/operations/dtos/add-operation.dto';
 import { HttpStatus } from '../../../src/presentation/helper/enums/http-status.enum';
 import { createAndLoginUser } from '../../helpers/user-functions-default';
 
@@ -36,5 +37,82 @@ describe('Add Recipe a User', () => {
     expect(body.data).toHaveProperty('balance');
     expect(body.data).toHaveProperty('totalRecipes');
     expect(body.data).toHaveProperty('totalExpenses');
+  });
+
+  it('GET - must return correct balance when add executed operation and fetching a user statement', async () => {
+    const { tokenToTest } = await createAndLoginUser();
+
+    const VALUE_TO_ADD = 50.0;
+    const EXPECTED_BALANCE = 100;
+
+    const recipeToAdd = {
+      type: 'recipe',
+      value: VALUE_TO_ADD,
+      executed: true
+    } as AddOperationDto;
+
+    await request(expressAplication)
+      .post('/finance/operations')
+      .send(recipeToAdd)
+      .set('Authorization', 'Bearer ' + tokenToTest);
+
+    await request(expressAplication)
+      .post('/finance/operations')
+      .send(recipeToAdd)
+      .set('Authorization', 'Bearer ' + tokenToTest);
+
+    const result = await request(expressAplication)
+      .get('/finance/extract')
+      .set('Authorization', 'Bearer ' + tokenToTest);
+
+    const { status, body } = result;
+    expect(status).toBe(HttpStatus.SUCCESS);
+    expect(body.data).toBeDefined();
+    expect(body.data).toHaveProperty('name');
+    expect(body.data).toHaveProperty('email');
+    expect(body.data.balance).toBe(EXPECTED_BALANCE);
+    expect(body.data.totalRecipes).toBe(EXPECTED_BALANCE);
+  });
+
+  it('GET - must return correct balance when add recipe and expense and fetching a user statement', async () => {
+    const { tokenToTest } = await createAndLoginUser();
+
+    const VALUE_TO_ADD = 50.0;
+    const EXPECTED_BALANCE = 0;
+
+    const recipeToAdd = {
+      type: 'recipe',
+      value: VALUE_TO_ADD,
+      executed: true
+    } as AddOperationDto;
+
+    await request(expressAplication)
+      .post('/finance/operations')
+      .send(recipeToAdd)
+      .set('Authorization', 'Bearer ' + tokenToTest);
+
+    const expenseToAdd = {
+      type: 'expense',
+      value: VALUE_TO_ADD,
+      executed: true
+    } as AddOperationDto;
+
+    await request(expressAplication)
+      .post('/finance/operations')
+      .send(expenseToAdd)
+      .set('Authorization', 'Bearer ' + tokenToTest);
+
+    const result = await request(expressAplication)
+      .get('/finance/extract')
+      .set('Authorization', 'Bearer ' + tokenToTest);
+
+    const { status, body } = result;
+    expect(status).toBe(HttpStatus.SUCCESS);
+    expect(body.data).toBeDefined();
+    expect(body.data).toHaveProperty('name');
+    expect(body.data).toHaveProperty('email');
+    expect(body.data.balance).toBe(EXPECTED_BALANCE);
+    expect(body.data.totalRecipes).toBe(VALUE_TO_ADD);
+    expect(body.data.totalExpenses).toBe(VALUE_TO_ADD);
   });
 });

18de98399c6ecf6ea6b3b1001671d5f8071f0fa8
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2023-02-03 18:09:21 -0400

    fix: change to hashRouter

--- web/src/routes/app.routes.tsx
+++ web/src/routes/app.routes.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { myFinancesRoute } from '../constants/routes.constants';
 import Dashboard from '../pages/MyFinances/MyFinances';
 
 const AppRoutes: React.FC = () => (
-  <BrowserRouter basename='/'>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Navigate to={myFinancesRoute} replace />} />
       <Route path="*" element={<Navigate to={myFinancesRoute} replace />} />
       <Route path={myFinancesRoute} element={<Dashboard />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default AppRoutes;
--- web/src/routes/auth.routes.tsx
+++ web/src/routes/auth.routes.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { loginRoute, signupRoute } from '../constants/routes.constants';
 import Login from '../pages/Login/Login';
 import Signup from '../pages/Signup/Signup';
 
 const AuthRoutes: React.FC = () => (
-  <BrowserRouter basename='/'>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Navigate to={loginRoute} replace />} />
       <Route path="*" element={<Navigate to={loginRoute} replace />} />
       <Route path={loginRoute} element={<Login />} />
       <Route path={signupRoute} element={<Signup />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default AuthRoutes;
